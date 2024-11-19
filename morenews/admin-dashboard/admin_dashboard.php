b273a067c81274f93920e9e592a986d705cd4bc4
Sean Murphy <sean@iamseanmurphy.com>
2024-11-19 21:26:40 +0000

    3.0.3

--- morenews/admin-dashboard/admin_dashboard.php
+++ morenews/admin-dashboard/admin_dashboard.php
@@ -92,6 +92,7 @@ if (!class_exists('AF_themes_info')) {
 
       //Add info page.
 
+      $upgrade_texts = __('Upgrade', 'morenews');
       $starter_template_slug = 'aft-block-patterns';
       $template_kits_slug = 'aft-template-kits';
       $starter_sites_order = 2;
@@ -164,9 +165,9 @@ if (!class_exists('AF_themes_info')) {
       add_submenu_page(
         'morenews', // Parent slug.
         __('Upgrade', 'morenews'), // Page title.
-        '<span id="aft-upgrade-menu-item">' . __('Upgrade', 'morenews') .'</span>', // Menu title.
-        capability: 'manage_options', // Capability.
-        menu_slug: esc_url('https://afthemes.com/products/morenews-pro/') // Menu slug.
+        '<span id="aft-upgrade-menu-item">' .$upgrade_texts.'</span>', // Menu title.
+        'manage_options', // Capability.
+        esc_url('https://afthemes.com/products/morenews-pro/') // Menu slug.
         
       );
     }
